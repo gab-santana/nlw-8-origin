6178d579804c372f2f94021c760564c3ed2d604b
gab-santana <gabrielsantana100@hotmail.com>
2022-05-11 16:07:17 -0300

    update

--- main.js
+++ main.js
@@ -4,25 +4,32 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
-  activateMenuAtCurrentSection()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
-function activateMenuAtCurrentSection() {
-  //linha alvo
+function activateMenuAtCurrentSection(section) {
   const targetLine = scrollY + innerHeight / 2
-  //Verificar se a seção passou da linha
-  const sectionTop = home.offsetTop //topo da seção home
-  const sectionHeight = home.offsetHeight // Altura da Home
-  const sectionTopReachOrTargetLine = targetLine >= sectionTop
-  //fim da seção
-  const sectionEndsAt = sectionTop + sectionHeight
-  //Fim da seção passou da linha
-  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
-  //limites da seção
-  const sectionBoundaries =
-    sectionTopReachOrTargetLine && !sectionEndPassedTargetLine
 
-  const sectionId = section.getAtribute('id')
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
   menuElement.classList.remove('active')
@@ -40,7 +47,7 @@ function showNavOnScroll() {
 }
 
 function showBackToTopButtonOnScroll() {
-  if (scrollY > 400) {
+  if (scrollY > 550) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
@@ -60,11 +67,11 @@ ScrollReveal({
   distance: '30px',
   duration: 700
 }).reveal(`#home, 
-#home img, 
-#home .stats, 
-#services, 
-#services header, 
-#services .card,
-#about,
-#about header,
-#about .content`)
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
